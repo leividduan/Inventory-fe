51b34db13c9c382b894cf1dcb9fef6181fdc09af
Deivid <deivid.cardos@gmail.com>
2023-05-21 15:30:34 -0300

    implement: login

---
+++ src/types/Input.ts
@@ -0,0 +1,10 @@
+import { InputError } from './InputError';
+
+export interface Input {
+  type: string,
+  id: string,
+  name: string,
+  placeholder: string,
+  required: boolean,
+  error?: InputError
+}

f5f01055ab7501f555ca2f9ae7fc21407f47fb78
Deivid <deivid.cardos@gmail.com>
2023-05-22 23:36:12 -0300

    fix: login

--- src/components/LayoutLogin.tsx
+++ src/components/LayoutLogin.tsx
@@ -8,7 +8,7 @@ function Layout({children}:LayoutType) {
   return (
     <section>
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             {children}
           </div>
